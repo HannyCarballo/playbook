b801e9a8c1f21e557d69a67927add4e4247d8248
Hey I´m Hanny <89166148+HannyCarballo@users.noreply.github.com>
2022-04-28 14:20:19 -0500

    Add files via upload

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,12 @@
+export default class Pokemon {
+    constructor(name) {
+        this.name = name 
+        }
+    
+        sayHello(saludo){
+          console.log(`Mi pokemon ${this.name} te saluda!!!`)
+        }
+        sayMessage(message){
+            console.log(`Mi pokemon ${this.name} dice: Heey!`)  
+        }
+  }
